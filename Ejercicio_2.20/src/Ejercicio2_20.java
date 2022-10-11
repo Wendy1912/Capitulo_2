dc2c210154f78d93e7885e8e0fa5654909ea53c8
Wendy1912 <115590830+Wendy1912@users.noreply.github.com>
2022-10-11 16:17:25 -0600

    Add files via upload

---
+++ Ejercicio_2.20/src/Ejercicio2_20.java
@@ -0,0 +1,18 @@
+//¿Qué imprime el siguiente código?
+
+public class Ejercicio2_20
+{
+    public static void main (String[]args)
+    {
+        System.out.println("*");
+        System.out.println("***");
+        System.out.println("*****");
+        System.out.println("****");
+        System.out.println("**");
+    }
+}
+//*
+//***
+//*****
+//****
+//**
