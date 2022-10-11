79f7fabee52687d6109bd6ee38b78ee1f345b5e4
Wendy1912 <115590830+Wendy1912@users.noreply.github.com>
2022-10-11 16:21:13 -0600

    Add files via upload

---
+++ Ejercicio_2.31/src/Ejercicio2_31.java
@@ -0,0 +1,22 @@
+//Ejercicio 2.31
+//Escriba una aplicación que calcule los cuadrados y cubos de los números del 0 al 10, y que imprima los valores resultantes en formato de tabla
+
+public class Ejercicio2_31
+{
+    public static void main(String[]args)
+    {
+        System.out.println("numero\tnumero\tnumero");
+        System.out.println("0\t0\t0");
+        System.out.println("1\t1\t1");
+
+        System.out.println("2\t" + 2*2 + "\t" + 2*2*2);
+        System.out.println("3\t" + 3*3 + "\t" + 3*3*3);
+        System.out.println("4\t" + 4*4 + "\t" + 4*4*4);
+        System.out.println("5\t" + 5*5 + "\t" + 5*5*5);
+        System.out.println("6\t" + 6*6 + "\t" + 6*6*6);
+        System.out.println("7\t" + 7*7 + "\t" + 7*7*7);
+        System.out.println("8\t" + 8*8 + "\t" + 8*8*8);
+        System.out.println("9\t" + 9*9 + "\t" + 9*9*9);
+        System.out.println("10\t" + 10*10 + "\t" + 10*10*10);
+    }
+}
